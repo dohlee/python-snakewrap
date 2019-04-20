8e0309d2b01fbf71d0e56fd71459ce49c25075ce
dohlee <apap950419@gmail.com>
2019-04-21 01:40:05 +0900

    Implement assignment in rulethreads.

--- src/snakewrap/rulethreads.py
+++ src/snakewrap/rulethreads.py
@@ -1,20 +1,23 @@
+from snakewrap import exception
 
 class RuleThreads():
     def __init__(self, command_key, name=None, desc=None):
         self.command_key = command_key
-        self.num_thread = None
+        self.num_threads = None
         self.name, self.desc = name, desc
-    
-    def attach(self, num_thread):
-        try:
-            self.num_thread = int(num_thread)
-        except ValueError:
-            raise TypeError('Please give integer to threads')
             
+    def assign(self, snakemake_threads):
+        raise NotImplementedError
 
     def __str__(self):
-        return '%s %d' % (self.command_key, self.num_thread)
+        return '%s %d' % (self.command_key, self.num_threads)
 
 class SimpleRuleThreads(RuleThreads):
     def __init__(self, command_key, name=None, desc=None):
         super(SimpleRuleThreads, self).__init__(command_key, name=None, desc=None)
+    
+    def assign(self, snakemake_threads):
+        try:
+            self.num_threads = int(snakemake_threads)
+        except ValueError:
+            raise TypeError('Please give integer for the number of threads.')
--- tests/test_rulethreads.py
+++ tests/test_rulethreads.py
@@ -2,34 +2,34 @@ import pytest
 
 from snakewrap import rulethreads
 
-def test_rulethreads_init():
-    rt = rulethreads.RuleThreads('-t')
+def test_simplerulethreads_init():
+    rt = rulethreads.SimpleRuleThreads('-t')
 
-def test_rulethreads_attach():
-    rt = rulethreads.RuleThreads('-t')
-    rt.attach(4)
+def test_simplerulethreads_assign():
+    rt = rulethreads.SimpleRuleThreads('-t')
+    rt.assign(4)
 
-def test_rulethreads_attach_failed_because_of_non_int_attachment():
-    rt = rulethreads.RuleThreads('-t')
-
-    with pytest.raises(TypeError):
-        rt.attach('a')
+def test_rulethreads_attach_failed_because_of_non_int_assignment():
+    rt = rulethreads.SimpleRuleThreads('-t')
 
     with pytest.raises(TypeError):
-        rt.attach([1, 2])
+        rt.assign('a')
+
+    with pytest.raises(TypeError):
+        rt.assign([1, 2])
     
     with pytest.raises(TypeError):
-        rt.attach({'thread': 4})
+        rt.assign({'thread': 4})
 
 def test_rulethreads_string():
-    rt = rulethreads.RuleThreads('-t')
-    rt.attach(4)
+    rt = rulethreads.SimpleRuleThreads('-t')
+    rt.assign(4)
 
     assert str(rt) == '-t 4'
 
-def test_rulethreads_attach_success_given_integer_convertible_string():
-    rt = rulethreads.RuleThreads('-t')
-    rt.attach('2')
+def test_rulethreads_assign_success_given_integer_convertible_string():
+    rt = rulethreads.SimpleRuleThreads('-t')
+    rt.assign('2')
 
     assert str(rt) == '-t 2'
 
